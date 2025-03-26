c8703d90470317a3eef38402eeb88c785b670b2f
Roovix <yourtom936@gmail.com>
2025-03-26 21:15:17 +0600

    Update fetch_users_data.js

--- dashboard_script/fetch_users_data.js
+++ dashboard_script/fetch_users_data.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     onAuthStateChanged(auth, async (user) => {
         if (!user) {
             // Redirect to login page if not logged in
-            window.location.href = "login.html";
+            window.location.href = "login";
             return;
         }
 
@@ -44,11 +44,51 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("user-photo").src = userInfo.profile_picture;
             document.getElementById("joined-date").textContent = `Joined at - ${formatISODate(userInfo.createdAt)}`;
             
+            if(userInfo.bio) {
+                document.getElementById("bio-content").textContent = userData.bio;
+            }else {
+                document.getElementById("bio-content").textContent = "No intro provided.";
+            }
+
             // Update user profile pic in navbar
             document.getElementById("profile-picture-on-dashboard-nam").src = userInfo.profile_picture;
             document.getElementById("profile-picture-on-market-nam").src = userInfo.profile_picture;
             document.getElementById("profile-picture-on-settings-nam").src = userInfo.profile_picture;
             document.getElementById("profile-picture-on-support-nam").src = userInfo.profile_picture;
+            
+
+            // Show interests and skills
+            const interestsList = document.getElementById('interests-list');
+            const skillsList = document.getElementById('skills-list');
+            
+            let interestsData = userData.interests;
+            let skillsData = userData.skills;
+
+            // Show interests
+            if(interestsData) {
+            interestsData = interestsData.split(',');
+            interestsData.forEach((interest)=>{
+                const interestItem = document.createElement('div');
+                interestItem.classList= 'interest-tag';
+                interestItem.textContent = interest;
+                interestsList.appendChild(interestItem);
+            })
+            }else{
+            interestsList.textContent = 'No interests found';
+            }
+
+            // Show skills
+            if(skillsData) {
+            skillsData = skillsData.split(',');
+            skillsData.forEach((skill)=>{
+                const skillItem = document.createElement('div');
+                skillItem.classList='interest-tag';
+                skillItem.textContent = skill;
+                skillsList.appendChild(skillItem);
+            })
+            }else{
+            skillsList.textContent = 'No skills found';
+            }
         } catch (error) {
             console.error("Error fetching user data:", error);
         }
