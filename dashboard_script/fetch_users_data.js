a0c30716e3df476202e1a69004a4c71e5bc0f32d
Roovix <yourtom936@gmail.com>
2025-03-29 14:45:21 +0600

    Update fetch_users_data.js

--- dashboard_script/fetch_users_data.js
+++ dashboard_script/fetch_users_data.js
@@ -1,10 +1,9 @@
 import { auth, db } from "https://www.roovix.com/config/firebase_config.js";
 import { ref, set, get } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-import { getDomain, formatISODate } from "https://element.roovix.com/functions/app.js";
+import { getDomain, formatISODate, ISoToTimeAgo } from "https://element.roovix.com/functions/app.js";
 
 
-// On load
 document.addEventListener("DOMContentLoaded", () => {
     // Wait for authentication state
     onAuthStateChanged(auth, async (user) => {
@@ -25,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("user-name").textContent = userInfo.username;
             document.getElementById("user-email").textContent = userInfo.email;
             document.getElementById("user-photo").src = userInfo.profile_picture;
-            document.getElementById("joined-date").textContent = `Joined at - ${formatISODate(userInfo.createdAt)}`;
+            document.getElementById("joined-date").textContent = `Joined - ${formatISODate(userInfo.createdAt)}`;
             
             if(userInfo.bio) {
                 document.getElementById("bio-content").textContent = userInfo.bio;
@@ -104,6 +103,33 @@ document.addEventListener("DOMContentLoaded", () => {
             }else {
                 linksList.textContent = 'No links found';
             }
+
+            // Show projects list
+            const projectList = document.getElementById("project-content-list");
+            if(userInfo.projects) {
+                let projects = userInfo.projects;
+                let projectHtml = ``;
+
+                // Loop inside projects to fetch project one by one
+                projects.forEach((project)=>{
+                    projectHtml += `
+                        <a href="${project.url}" class="content-item">
+                            <div class="content-thumbnail">
+                                <img src="image_assets/success_right.webp" alt="">
+                            </div>
+                            <div class="content-details">
+                                <div class="content-title">${project.title}</div>
+                                <div class="content-tags">${project.tag_line}</div>
+                            </div>
+                            <div class="content-time">${ISoToTimeAgo(project.date)}</div>
+                        </a>
+                    `;
+                });
+                projectList.innerHTML = projectHtml;
+            }else {
+                projectList.textContent = "No projects sources found";
+            }
+            
         } catch (error) {
             console.error("Error fetching user data:", error);
         }
