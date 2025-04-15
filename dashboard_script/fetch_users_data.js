54c23f85a3dab38ad82e6696d03c21f935408937
Roovix <yourtom936@gmail.com>
2025-04-15 23:37:53 +0600

    Update fetch_users_data.js

--- dashboard_script/fetch_users_data.js
+++ dashboard_script/fetch_users_data.js
@@ -2,7 +2,7 @@ import { auth, db } from "https://www.roovix.com/config/firebase_config.js";
 import { ref, set, get } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 import { getDomain, formatISODate, ISoToTimeAgo } from "https://element.roovix.com/functions/app.js";
-import { deletePopup, confirmPopup, noticePopup, addPopupStyles } from "https://element.roovix.com/functions/popups.js";
+import { deletePopup, confirmPopup, noticePopup } from "https://element.roovix.com/functions/popups.js";
 
 
 
