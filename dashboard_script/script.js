93542fd78ed5a1fb3c9c5d81c04137deafbd850e
Roovix <yourtom936@gmail.com>
2025-04-15 23:21:09 +0600

    Update script.js

--- dashboard_script/script.js
+++ dashboard_script/script.js
@@ -1,18 +1,3 @@
-class Chart {
-  constructor(ctx, config) {
-    this.ctx = ctx
-    this.config = config
-
-    // Basic implementation to avoid errors.  A real chart library would do much more.
-    this.render = () => {
-      console.log("Rendering chart with config:", config)
-    }
-
-    this.destroy = () => {
-      console.log("Destroying chart")
-    }
-  }
-}
 
 document.addEventListener("DOMContentLoaded", () => {
   // open post popup
